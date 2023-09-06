60f3770328b976a525e571f75c0468e5741b6ba9
Krishna Meghwal <krishnameghwal635@gmail.com>
2023-09-06 12:59:32 +0530

    Cart

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
 import RootLayout from "./layout/RootLayout";
 import Login from "./features/user/Login";
 import Register from "./features/user/Register";
-
+import Cart from "./features/cart/Cart";
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -19,6 +19,7 @@ function App() {
         <Route index element={<Main />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
       </Route>
     )
   );
